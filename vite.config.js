bb74918643cd0edd416c66fc8cbb0fed117eae03
David Brunni <davidbrunni@hotmail.com>
2025-05-19 16:13:30 +0200

    Ändra sökväg

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: "/Grupparbete-Pilgiftsgrodorna/",
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': '/src',
-        },
+  base: "/Grupparbete-Pilgiftsgrodorna/",
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: '@import "../styles/partials/_variables.scss";',
-            },
-        },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "../styles/partials/_variables.scss";',
+      },
     },
+  },
 });
